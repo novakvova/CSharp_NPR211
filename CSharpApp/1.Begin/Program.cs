3d85ae76045b340a043c15a4494f78362d15b9b2
novakvova <novakvova@gmail>
2023-09-10 13:12:14 +0300

    Calculator begin

--- CSharpApp/1.Begin/Program.cs
+++ CSharpApp/1.Begin/Program.cs
@@ -10,9 +10,55 @@ namespace _1.Begin
             Console.InputEncoding = Encoding.UTF8;
             Console.OutputEncoding = Encoding.UTF8;
 
-            Console.WriteLine("Я додав функцію Main :)");
+            //Console.WriteLine("Я додав функцію Main :)");
+            int action = -1;
+            do
+            {
+                Console.WriteLine("Оберіть дію:");
+                Console.WriteLine("0. Вихід");
+                Console.WriteLine("1. Додати");
+                Console.WriteLine("2. Відняти");
+                Console.WriteLine("3. Помножити");
+                Console.WriteLine("4. Поділити");
+                Console.Write("->_");
+                if (!int.TryParse(Console.ReadLine(), out action))
+                {
+                    action = -1;
+                    continue;
+                }
+                //action = int.Parse(Console.ReadLine());
+                switch(action)
+                {
+                    case 1:
+                        {
+                            InputValue(out int a, out int b);
+                            int result = a + b;
+                            Console.WriteLine("Результат: "+result);
+                            break;
+                        }
+                    case 2:
+                        {
+                            //int a, b;
+                            InputValue(out int a, out int b);
+                            int result = a - b;
+                            Console.WriteLine("Результат: " + result);
+                            break;
+                        }
+
+                }
+
+            } while (action!=0);
+           
 
             Console.ReadLine();
+        }
+        //ref, out
+        public static void InputValue(out int a, out int b)
+        {
+            Console.WriteLine("Вкажіть значення a");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Вкажіть значення b");
+            b = int.Parse(Console.ReadLine());
         }
     }
 }
