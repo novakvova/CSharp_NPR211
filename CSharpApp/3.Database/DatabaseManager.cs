725291ebc20f2380ec20318731b85a720ff81d9b
novakvova <novakvova@gmail>
2023-09-24 10:20:21 +0300

    Add Configuration file

---
+++ CSharpApp/3.Database/DatabaseManager.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Database
+{
+    public class DatabaseManager : IDisposable
+    {
+        private readonly SqlConnection _conn;
+
+        public DatabaseManager()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            IConfiguration configuration = builder.Build();
+
+            string conStr = configuration.GetConnectionString("MSSQLServerConnection") ?? "Data Source=.;Integrated Security=True;";
+
+            _conn = new SqlConnection(conStr);
+            _conn.Open();
+        }
+        
+        public void CraateDatabase()
+        {
+            Console.WriteLine("Вкажіть назву бази даних:");
+            string dbName = Console.ReadLine();
+
+            var isEsixt = IsExistDatabase(dbName);
+            if (isEsixt)
+            {
+                Console.WriteLine("База даних {0} уже існує", dbName);
+                return;
+            }
+            //створення на сервері бази даних
+            //string sql = "CREATE DATABASE "+dbName+";";
+            string sql = $"CREATE DATABASE {dbName};";
+            SqlCommand sqlCommand = _conn.CreateCommand(); //окманди виконуєються на основі підлкючення
+            sqlCommand.CommandText = sql; //текст команди
+            //виконати комнаду до сервера
+            sqlCommand.ExecuteNonQuery();
+            Console.WriteLine("Успішно створено БД :)");
+        }
+
+        /// <summary>
+        /// Перевірка існування БД на сервері
+        /// </summary>
+        /// <param name="name">Назва бази</param>
+        /// <param name="conStr">Строка підлкючення</param>
+        /// <returns>Повертає bool, True - якщо існує</returns>
+        private bool IsExistDatabase(string name)
+        {
+            //Перевірка чи існує база даних
+            string sql = $"IF EXISTS (SELECT name FROM sys.databases WHERE name = '{name}')" +
+                " SELECT 1 AS Result" +
+                " ELSE" +
+                " SELECT 0 AS Result";
+            SqlCommand cmd = _conn.CreateCommand();
+            cmd.CommandText = sql;
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    int result = int.Parse(reader["Result"].ToString());
+                    if (result > 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public void ShowAllDatabase()
+        {
+            //показати список БД
+            string sql = "SELECT name FROM sys.databases WHERE database_id > 4;";
+            SqlCommand sqlCommand = _conn.CreateCommand();
+            sqlCommand.CommandText = sql;
+            //Результа сервера будемо читати через SqlDataReeader
+            using (SqlDataReader reader = sqlCommand.ExecuteReader())
+            {
+                Console.WriteLine("Списко база даних:");
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["name"]);
+                }
+            }
+        }
+
+        public void DeleteDatabase()
+        {
+            Console.WriteLine("Вкажіть назву бази даних:");
+            string dbName = Console.ReadLine();
+
+
+            var isEsixt = IsExistDatabase(dbName);
+            if (!isEsixt)
+            {
+                Console.WriteLine("База даних не існує", dbName);
+                return;
+            }
+            string sql = $"DROP DATABASE {dbName};";
+            SqlCommand sqlCommand = _conn.CreateCommand(); //окманди виконуєються на основі підлкючення
+            sqlCommand.CommandText = sql; //текст команди
+            //виконати комнаду до сервера
+            sqlCommand.ExecuteNonQuery();
+            Console.WriteLine("Успішно видалено БД :)");
+        }
+
+        public void Dispose()
+        {
+            _conn.Close();
+        }
+    }
+}
--- CSharpApp/3.Database/Program.cs
+++ CSharpApp/3.Database/Program.cs
@@ -9,14 +9,13 @@ namespace _3.Database
         {
             Console.InputEncoding = Encoding.UTF8;
             Console.OutputEncoding = Encoding.UTF8;
-            //Підлкючення до сервера
-            //string conStr = "Data Source=.;Integrated Security=True;";
-            //string conStr = "Server=20.65.144.204;User=kaban;Password=9[nV`e7VN`0%;";
-            //string conStr = "Data Source=20.65.144.204;User=kaban;Password=9[nV`e7VN`0%;";
-            string conStr = "Data Source=20.65.144.204;User ID=kaban;Password=9[nV`e7VN`0%;";
-
-            SqlConnection con = new SqlConnection(conStr);
-            con.Open();
+            ///Підлкючення до сервера
+            ///string conStr = "Data Source=.;Integrated Security=True;";
+            ///string conStr = "Server=20.65.144.204;User=kaban;Password=9[nV`e7VN`0%;";
+            ///string conStr = "Data Source=20.65.144.204;User=kaban;Password=9[nV`e7VN`0%;";
+           
+            
+            DatabaseManager databaseManager = new DatabaseManager();
             int action = 0;
             do
             {
@@ -30,113 +29,25 @@ namespace _3.Database
                 {
                     case 1:
                         {
-                            CraateDatabase(con);
+                            databaseManager.CraateDatabase();
                             break;
                         }
                     case 2:
                         {
-                            ShowAllDatabase(con);
+                            databaseManager.ShowAllDatabase();
                             break;
                         }
                     case 3:
                         {
-                            DeleteDatabase(con);
+                            databaseManager.DeleteDatabase();
                             break;
                         }
                 }
             } while (action!=0);
-            con.Close();
 
-            //Console.WriteLine("Привіт козак!");
+            databaseManager.Dispose();
         }
 
-        private static void CraateDatabase(SqlConnection con)
-        {
-            Console.WriteLine("Вкажіть назву бази даних:");
-            string dbName = Console.ReadLine();
-
-
-            var isEsixt = IsExistDatabase(dbName, con);
-            if (isEsixt)
-            {
-                Console.WriteLine("База даних {0} уже існує", dbName);
-                return;
-            }
-            //створення на сервері бази даних
-            //string sql = "CREATE DATABASE "+dbName+";";
-            string sql = $"CREATE DATABASE {dbName};";
-            SqlCommand sqlCommand = con.CreateCommand(); //окманди виконуєються на основі підлкючення
-            sqlCommand.CommandText = sql; //текст команди
-            //виконати комнаду до сервера
-            sqlCommand.ExecuteNonQuery();
-            Console.WriteLine("Успішно створено БД :)");
-        }
-
-        /// <summary>
-        /// Перевірка існування БД на сервері
-        /// </summary>
-        /// <param name="name">Назва бази</param>
-        /// <param name="conStr">Строка підлкючення</param>
-        /// <returns>Повертає bool, True - якщо існує</returns>
-        private static bool IsExistDatabase(string name, SqlConnection con)
-        {
-            //Перевірка чи існує база даних
-            string sql = $"IF EXISTS (SELECT name FROM sys.databases WHERE name = '{name}')" +
-                " SELECT 1 AS Result" +
-                " ELSE" +
-                " SELECT 0 AS Result";
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandText = sql;
-            using (SqlDataReader reader = cmd.ExecuteReader())
-            {
-                if (reader.Read())
-                {
-                    int result = int.Parse(reader["Result"].ToString());
-                    if (result > 0)
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-    
-        private static void ShowAllDatabase(SqlConnection con)
-        {
-            //показати список БД
-            string sql = "SELECT name FROM sys.databases WHERE database_id > 4;";
-            SqlCommand sqlCommand = con.CreateCommand();
-            sqlCommand.CommandText = sql;
-            //Результа сервера будемо читати через SqlDataReeader
-            using (SqlDataReader reader = sqlCommand.ExecuteReader())
-            {
-                Console.WriteLine("Списко база даних:");
-                while (reader.Read())
-                {
-                    Console.WriteLine(reader["name"]);
-                }
-            }
-        }
-
-
-        private static void DeleteDatabase(SqlConnection con)
-        {
-            Console.WriteLine("Вкажіть назву бази даних:");
-            string dbName = Console.ReadLine();
-
-
-            var isEsixt = IsExistDatabase(dbName, con);
-            if (!isEsixt)
-            {
-                Console.WriteLine("База даних не існує", dbName);
-                return;
-            }
-            string sql = $"DROP DATABASE {dbName};";
-            SqlCommand sqlCommand = con.CreateCommand(); //окманди виконуєються на основі підлкючення
-            sqlCommand.CommandText = sql; //текст команди
-            //виконати комнаду до сервера
-            sqlCommand.ExecuteNonQuery();
-            Console.WriteLine("Успішно видалено БД :)");
-        }
+        
     }
 }
