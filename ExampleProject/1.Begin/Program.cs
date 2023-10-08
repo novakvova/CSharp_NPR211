7a19aca797401f583aa62dc69e5040b1c991b673
novakvova <novakvova@gmail>
2023-10-08 09:38:15 +0300

    Begin wokring c#

---
+++ ExampleProject/1.Begin/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace _1.Begin
+{
+    class Program
+    {
+        public static void Main(string []args)
+        {
+            //CTS - загально прийняті стандартні типи даних. Які є у всіх мовах на .NET
+            //CLR - Середовище виконання
+            Console.InputEncoding=Encoding.UTF8;
+            Console.OutputEncoding=Encoding.UTF8;
+            //string,
+            
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Привіт!");
+        }
+    }
+}
+
