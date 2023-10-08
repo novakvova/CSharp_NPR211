c2ff595e9983e3ea582252e9e8ddda5a93a45fd2
novakvova <novakvova@gmail>
2023-10-08 10:58:41 +0300

    Is Good Working app

--- ExampleProject/1.Begin/Program.cs
+++ ExampleProject/1.Begin/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Text;
 
 namespace _1.Begin
 {
@@ -7,13 +8,26 @@ namespace _1.Begin
         public static void Main(string []args)
         {
             //CTS - загально прийняті стандартні типи даних. Які є у всіх мовах на .NET
+            //System.Byte,  SByte, Intl6, Int32, Int64, UIntl6, UInt32, UInt64,
+            //Single, Double, Object, Char, String, Decimal, Boolean
             //CLR - Середовище виконання
             Console.InputEncoding=Encoding.UTF8;
             Console.OutputEncoding=Encoding.UTF8;
+             //CultureInfo culture = new CultureInfo("uk-ua");
+            CultureInfo culture = new CultureInfo("en");
             //string,
+            // DateTime dt = DateTime.Now;
+            DateTime dt = new DateTime(2004,6,30);
             
-            // See https://aka.ms/new-console-template for more information
-            Console.WriteLine("Привіт!");
+            string str = dt.ToString("D", culture);
+            Console.WriteLine("Зараз час "+ str);
+            //
+            // Object hello=12;
+            // Console.WriteLine("Вкажіть значення: ");
+            // hello = Convert.ToDecimal(Console.ReadLine(), culture);
+            // string str = hello.ToString();
+            // Console.WriteLine("Ви вказали: " + str);
+            // Console.WriteLine("sizeof(byte) = {0}", sizeof(byte));
         }
     }
 }
