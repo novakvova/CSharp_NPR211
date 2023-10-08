9ab697eea260d0a10073fa7e220214d44230ebf0
novakvova <novakvova@gmail>
2023-10-08 13:16:45 +0300

    Beging ILSpy

--- ExampleProject/1.Begin/Program.cs
+++ ExampleProject/1.Begin/Program.cs
@@ -5,22 +5,41 @@ namespace _1.Begin
 {
     class Program
     {
-        public static void Main(string []args)
+        public static int Main(string []args)
         {
             //CTS - загально прийняті стандартні типи даних. Які є у всіх мовах на .NET
             //System.Byte,  SByte, Intl6, Int32, Int64, UIntl6, UInt32, UInt64,
             //Single, Double, Object, Char, String, Decimal, Boolean
             //CLR - Середовище виконання
+            Console.Title = "Козакі";
+            
+            //string, 
+            //StringBuilder
+            //int a = default;
+            Console.WriteLine("double = {0}",double.Epsilon);
+            Console.WriteLine("double = {0:f324}",double.Epsilon);
             Console.InputEncoding=Encoding.UTF8;
             Console.OutputEncoding=Encoding.UTF8;
-             //CultureInfo culture = new CultureInfo("uk-ua");
-            CultureInfo culture = new CultureInfo("en");
+            int c = 26;
+            string str = $"Сьогодні температура {c}";
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Ваші параметри {0} - {1}", i, args[i]);
+            }
+
+            Console.ReadKey();
+            
+            // String name="Vova";
+            // Console.WriteLine("Name = {0}", name);
+            // CultureInfo culture = new CultureInfo("uk-ua");
+            //CultureInfo culture = new CultureInfo("en");
             //string,
             // DateTime dt = DateTime.Now;
-            DateTime dt = new DateTime(2004,6,30);
+            // DateTime dt = new DateTime(2004,6,30, 13,24,50);
             
-            string str = dt.ToString("D", culture);
-            Console.WriteLine("Зараз час "+ str);
+            //string str = dt.ToString("D", culture);
+            // string str = dt.ToString(culture);
+            // Console.WriteLine("Зараз час "+ str);
             //
             // Object hello=12;
             // Console.WriteLine("Вкажіть значення: ");
@@ -28,6 +47,7 @@ namespace _1.Begin
             // string str = hello.ToString();
             // Console.WriteLine("Ви вказали: " + str);
             // Console.WriteLine("sizeof(byte) = {0}", sizeof(byte));
+            return 0;
         }
     }
 }
