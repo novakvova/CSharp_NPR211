4fee0eb5e726ff7968ac50ca40e2df0cc35dbfc2
novakvova <novakvova@gmail>
2023-10-15 09:34:24 +0300

    Generation random data

---
+++ ExampleProject/2.Operators/MyUserInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Operators
+{
+    public class MyUserInfo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName} '\n-------------------" +
+                $"{Description}" +
+                $"\n-------------------'";
+        }
+    }
+}
---
+++ ExampleProject/2.Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿using Bogus;
+using System.Text;
+
+namespace _2.Operators
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Привіт козаки!");
+
+            GenerationData();
+
+            return 0;
+        }
+        private static void GenerationData(string local="uk")
+        {
+            var userFaker = new Faker<MyUserInfo>(local)
+                .RuleFor(x => x.FirstName, f => f.Name.FirstName())
+                .RuleFor(x => x.LastName, f => f.Name.LastName())
+                .RuleFor(x => x.Description, f => f.Lorem.Lines(15));
+
+            MyUserInfo user = userFaker.Generate();
+            Console.WriteLine(user);
+        }
+
+        
+    }
+}
