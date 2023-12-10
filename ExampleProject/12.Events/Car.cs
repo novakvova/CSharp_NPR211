351a8ddce5db421244e13ccee8ec1627528b6425
novakvova <novakvova@gmail>
2023-12-10 10:16:16 +0200

    Add Event

---
+++ ExampleProject/12.Events/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Events
+{
+    public class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+        //Поломка авто
+        private bool carIdDead;
+
+        public Car() { }
+        public Car(int currentSpeed, int maxSpeed, string petName)
+        {
+            CurrentSpeed = currentSpeed;
+            MaxSpeed = maxSpeed;
+            PetName = petName;
+        }
+
+        public delegate void CarEventDelegate(string message);
+
+        //Спрацьовує коли авто зломаний
+        public event CarEventDelegate CarEvent;
+        //Спроцьовує перед пломкой - за 10 км.
+        public event CarEventDelegate CarEventAbout;
+
+        public void Accelerate(int delta)
+        {
+            if(carIdDead)
+            {
+                if(CarEvent != null)
+                {
+                    CarEvent($"Вибач, але авто ви зломали... Завмер на відмітці {CurrentSpeed}");
+                }
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                if(10==MaxSpeed-CurrentSpeed && CarEventAbout!=null)
+                {
+                    CarEventAbout($"Не газуй більше, бо буде поломка --{CurrentSpeed}--");
+                }
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIdDead = true;
+                }
+                else
+                    Console.WriteLine($"Поточна швидкість = {CurrentSpeed}");
+            }
+        }
+
+
+
+    }
+}
---
+++ ExampleProject/12.Events/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace _12.Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Car bmw = new Car(10,220,"BMW X3");
+            bmw.CarEvent += GetInfo_CarEvent;
+            bmw.CarEventAbout += Bmw_CarEventAbout;
+
+            for (int i = 0; i < 7; i++) 
+            {
+                bmw.Accelerate(50);
+            }
+        }
+
+        private static void Bmw_CarEventAbout(string message)
+        {
+            var defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"-----{message}------");
+            Console.ForegroundColor = defaultColor;
+        }
+
+        private static void GetInfo_CarEvent(string message)
+        {
+            var defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"-----{message}------");
+            Console.ForegroundColor = defaultColor;
+        }
+
+
+    }
+}
